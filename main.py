ce8c4ceca1dab7b70e50e8003c9519c6eb4345b2
Jonathan Ferreira <jgferreira@wpi.edu>
2019-12-24 11:15:41 -0800

    Started work on filling CSV
    I feel like I should be using the CSV library, but oh well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import libraries
 import urllib.request
-import csv
+# import csv
+# TODO: Actually use the CSV library maybe?
 
 # # # Scrape pokedex into a text file
 # webPage = urllib.request.urlopen("https://www.smogon.com/dex/ss/pokemon/")
@@ -20,8 +21,8 @@ for line in fileIn:
 fileIn.close()
 fileOut.close()
 
-# pull raw data from fout into csv
-pokedex = []
+# Declare necessary variables
+# TODO: Rename these 
 var = ""
 var1 = ""
 var2 = ""
@@ -40,21 +41,41 @@ evoList = []
 alts = ""
 altsList = []
 
+# Handle File I/O
 secondFileIn = open("fout.txt","rt")
-formatedPokedex = open("formattedPokedex.csv","wb+")
+formatedPokedex = open("formattedPokedex.csv","w+")
+
+# Define and add CSV header fields
+fieldNames = ['Dex Number', 'Name', 'Type', 'Abilities', 'Health', 'Attack', 
+              'Defense', 'Special Attack', 'Special Defense', 'Speed', 'Weight',
+              'Height', 'Other forms', 'Evolutions']
+for i in range(0,len(fieldNames)):
+  if not i == 13:
+    formatedPokedex.write(fieldNames[i] + ',')
+  else:
+    formatedPokedex.write(fieldNames[i] + '\n')
+
+# Fill CSV
+# TODO: Turn these into proper functions
 for line in secondFileIn:
   if line.startswith("{"):
+    # # Not sure what dialect is
+    # writer = csv.writer(formatedPokedex,dialect='excel')
     # Get pokemon's name
-    var = line[line.find('"name":')+8 : line.find(",")-1]
+    var = line[line.find('"name":') + 8 : line.find(",") - 1]
+        # formatedPokedex.write(var + ',')
     print(var)
     # Get pokemon's health
     var1 = line[line.find('"hp"') + 5 : line.find(',"atk"')]
+        # formatedPokedex.write(var1 + ',')
     print(var1)
     # Get pokemon's attack
     var2 = line[line.find('"atk":') + 6 : line.find(',"def"')]
+        # formatedPokedex.write(var2 + '\n')
     print(var2)
     # Get pokemon's defense
     var3 = line[line.find('"def":') + 6 : line.find(',"spa"')]
+        # formatedPokedex.write(var3)
     print(var3)
     # Get pokemon's special attack
     var4 = line[line.find('"spa":') + 6 : line.find(',"spd"')]
@@ -83,12 +104,11 @@ for line in secondFileIn:
     dexNumber = line[line.find('dex_number') + 12 : line.find('cap') - 2]
     print(dexNumber)
     # Get pokemon's evolutions
-    evo = line[line.find('evos') + 8 : line.find('alts') - 4]
+    evo = line[line.find('evos') + 7 : line.find('alts') - 3]
     evoList = evo.split(",")
     print(evoList)
     # Get pokemon's other forms
     alts = line[line.find('alts') + 7 : line.find('genfamily') - 3]
     altsList = alts.split(",")
     print(altsList)
-
     break
